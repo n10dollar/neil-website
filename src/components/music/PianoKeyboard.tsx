93ea5c90168124016ef86bd08811c9bb4841c831
Neil <ntendolkar@berkeley.edu>
2023-06-19 15:19:59 -0700

    began state hook tracking which key is pressed

--- src/components/music/PianoKeyboard.tsx
+++ src/components/music/PianoKeyboard.tsx
@@ -1,5 +1,6 @@
 import Keys from "src/utils/piano-key-map.json";
 import { Layer, Rect, Stage } from "react-konva";
+import { useState } from "react";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -8,6 +9,7 @@ const yStart = 20;
 
 const octaves = 2;
 const whiteKeysPerOctave = 7;
+const keysPerOctave = 12;
 
 const whiteHeightWidthRatio = 6 / (7 / 8);
 const blackHeightWidthRatio = 3.5 / (1 / 2);
@@ -18,16 +20,27 @@ const blackKeyWidth = (60 * 4) / 7;
 const blackKeyHeight = blackKeyWidth * blackHeightWidthRatio;
 
 const PianoKeyboard = () => {
+  const [pressed, setPressed] = useState([
+    ...Array(octaves * keysPerOctave).fill(false),
+  ]);
+
   return (
     <Stage width={width} height={height}>
       <Layer zIndex={0}>
         {[...Array(octaves * whiteKeysPerOctave).keys()].map((key) => (
           <Rect
+            onClick={() =>
+              setPressed(
+                pressed.map((keyPressed, index) =>
+                  index === key ? true : keyPressed
+                )
+              )
+            }
             x={xStart + key * whiteKeyWidth}
             y={yStart}
             width={whiteKeyWidth}
             height={whiteKeyHeight}
-            fill={"lightgray"}
+            fill={!pressed[key] ? "lightgray" : "blue"}
             stroke={"black"}
             strokeWidth={1}
           />
@@ -37,6 +50,13 @@ const PianoKeyboard = () => {
         {[...Array(octaves * whiteKeysPerOctave).keys()].map((key) =>
           (key + 5) % 7 === 0 || (key + 1) % 7 === 0 ? null : (
             <Rect
+              onClick={() =>
+                setPressed(
+                  pressed.map((keyPressed, index) =>
+                    index === key ? true : keyPressed
+                  )
+                )
+              }
               x={xStart + (key + 1) * whiteKeyWidth - blackKeyWidth / 2}
               y={yStart}
               width={blackKeyWidth}
