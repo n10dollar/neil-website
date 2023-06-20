861ef867a9a6f09d77de2deec45a8ba39866373b
Neil <ntendolkar@berkeley.edu>
2023-06-20 14:58:51 -0700

    finished <PianoKeyboard> frame and clicking functionality

--- src/components/music/PianoKeyboard.tsx
+++ src/components/music/PianoKeyboard.tsx
@@ -1,11 +1,15 @@
-import Keys from "src/utils/piano-key-map.json";
 import { Layer, Rect, Stage } from "react-konva";
 import { useState } from "react";
 
-const width = window.innerWidth;
-const height = window.innerHeight;
-const xStart = 20;
-const yStart = 20;
+interface Props {
+  whiteKeyWidth: number;
+  whiteKeyColor?: string;
+  blackKeyColor?: string;
+  onClick: () => NonNullable<unknown>;
+}
+
+const xStart = 0;
+const yStart = 0;
 
 const octaves = 2;
 const whiteKeysPerOctave = 7;
@@ -17,20 +21,38 @@ const blackHeightWidthRatio = 3.5 / (1 / 2);
 
 const whiteKeyWidth = 60;
 const whiteKeyHeight = whiteKeyWidth * whiteHeightWidthRatio;
-const blackKeyWidth = (60 * 4) / 7;
+const blackKeyWidth = whiteKeyWidth * (4 / 7);
 const blackKeyHeight = blackKeyWidth * blackHeightWidthRatio;
 
-const PianoKeyboard = () => {
-  // const [whitePressed, setWhitePressed] = useState([
-  //   ...Array(octaves * whiteKeysPerOctave).fill(false),
-  // ]);
-  // const [blackPressed, setBlackPressed] = useState([
-  //   ...Array(octaves * blackKeysPerOctave).fill(false),
-  // ]);
+const whiteIndex = (key: number) => {
+  return key * 2 - Math.floor((key + 4) / 7) - Math.floor(key / 7);
+};
+const blackIndex = (key: number) => {
+  return 1 + key * 2 + Math.floor((key + 3) / 5) + Math.floor(key / 5);
+};
+const whitePosition = (key: number) => {
+  return key * whiteKeyWidth;
+};
+const blackPosition = (key: number) => {
+  return (
+    (key + 1 + Math.floor((key + 3) / 5) + Math.floor(key / 5)) *
+      whiteKeyWidth -
+    blackKeyWidth / 2
+  );
+};
 
+const PianoKeyboard = ({
+  whiteKeyWidth,
+  whiteKeyColor = "white",
+  blackKeyColor = "black",
+  onClick,
+}: Props) => {
   const [pressed, setPressed] = useState([
     ...Array(octaves * keysPerOctave).fill(false),
   ]);
+
+  const width = octaves * whiteKeysPerOctave * whiteKeyWidth;
+  const height = octaves * whiteKeyHeight;
 
   return (
     <Stage width={width} height={height}>
@@ -38,40 +60,33 @@ const PianoKeyboard = () => {
         {[...Array(octaves * whiteKeysPerOctave)].map((_, index) => (
           <Rect
             onPointerClick={() =>
-              setWhitePressed(whitePressed.map((_, pIndex) => pIndex === index))
+              setPressed(pressed.map((_, i) => i === whiteIndex(index)))
             }
-            x={xStart + index * whiteKeyWidth}
             y={yStart}
             width={whiteKeyWidth}
             height={whiteKeyHeight}
-            fill={!whitePressed[index] ? "lightgray" : "blue"}
+            fill={whiteKeyColor}
             stroke={"black"}
             strokeWidth={1}
+            x={xStart + whitePosition(index)}
+            {...(pressed[whiteIndex(index)] ? onClick() : null)}
           />
         ))}
       </Layer>
       <Layer>
-        {blackPressed.map((_, index) => (
+        {[...Array(octaves * blackKeysPerOctave)].map((_, index) => (
           <Rect
-            onPointerClick={() => {
-              setBlackPressed(blackPressed.map((_, index) => pIndex === index));
-              console.log(blackPressed);
-            }}
-            x={
-              xStart +
-              (index +
-                (Math.floor((index + 3) / 5) +
-                  Math.floor((index + 1) / 5) +
-                  1)) *
-                whiteKeyWidth -
-              blackKeyWidth / 2
+            onPointerClick={() =>
+              setPressed(pressed.map((_, i) => i === blackIndex(index)))
             }
             y={yStart}
             width={blackKeyWidth}
             height={blackKeyHeight}
-            fill={!blackPressed[index] ? "black" : "blue"}
+            fill={blackKeyColor}
             stroke={"black"}
             strokeWidth={1}
+            x={xStart + blackPosition(index)}
+            {...(pressed[blackIndex(index)] ? onClick() : null)}
           />
         ))}
       </Layer>
--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,12 +1,19 @@
 import PianoKeyboard from "components/music/PianoKeyboard.tsx";
+import { Center } from "@chakra-ui/react";
+
+const keyClick = () => {
+  return { fill: "blue" };
+};
 
 const Music = () => {
-    return (
-        <>
-            <div>Music</div>
-            <PianoKeyboard />
-        </>
-    );
+  return (
+    <>
+      <div>Music</div>
+      <Center>
+        <PianoKeyboard whiteKeyWidth={60} onClick={keyClick} />
+      </Center>
+    </>
+  );
 };
 
 export default Music;
